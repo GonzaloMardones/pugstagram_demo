37adb4a1c54b1b0b1cd8feeac30ad6435b94a26d
Gonzalo Mardones <46328656+GonzaloMardones@users.noreply.github.com>
2020-08-30 17:28:03 -0400

    project

---
+++ src/components/Footer.svelte
@@ -0,0 +1,17 @@
+<!-- Footer.svelte -->
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
+
+<div class="Footer">
+    <div class="Footer-container">
+      <small class="Footer-copy">
+         <h1>Pugstagram from platzi</h1>  
+      </small>
+    </div>
+  </div>
---
+++ src/components/Timeline.svelte
@@ -0,0 +1,24 @@
+<script>
+    import Card from './Card.svelte';
+    import Loading from './Loading.svelte'
+    export let posts = []
+</script>
+
+<!-- TimeLine.svelte -->
+<style>
+    .Timeline {
+      padding: 4em 0 0 0;
+    }
+  </style>
+  
+
+<div class="Timeline">
+    <div class="Timeline-container">
+      {#each posts as post}
+        <Card {...post}/>
+      {:else}
+        <Loading />
+      {/each}
+        
+    </div>
+</div>

72931c1a80cc899988da7ed9f2361fe8945ee271
Gonzalo Mardones <46328656+GonzaloMardones@users.noreply.github.com>
2020-09-11 08:00:52 -0300

    sapper

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,11 @@
 <script>
+  import { goto } from '@sapper/app'
   import {likeCount} from '../store/store';
+  export let segment;
+
+  const navigateHome = async ( )=> {
+    await goto('/') 
+  }
 </script>
 
 <!-- Header.svelte -->
@@ -59,13 +65,17 @@
       top: 0;
       left: 0;
     }
+
+  [aria-current] i {
+    color: #bc1888;
+  }
   </style>
 
 <div class="Header">
     <div class="Header-container">
         <div class="Header-content">
             <div class="Header-logo">
-                <h1>Pubstagram</h1>
+                <h1 on:click={navigateHome}>Pubstagram</h1>
             </div>
             <div class="Header-nav">
                 <ul>
@@ -74,7 +84,11 @@
                         {$likeCount === 0? '': $likeCount} 
                     </li>
                     <li>
+                      <a aria-current={segment=='profile'? 'page': undefined} 
+                         rel="prefetch"
+                         href="profile ">
                         <i class="fas fa-user-alt"/>
+                      </a>
                     </li>
                 </ul>
             </div>
